d35efb37eb308948b26f87ba92b0c1d996481b18
Ragnar Kaasik <ragnar.kaasik@gmail.com>
2023-11-30 11:55:18 +0200

    Initial commit

---
+++ drawroom/src/main/java/com/example/drawroom/DrawroomBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.drawroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawroomBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrawroomBackApplication.class, args);
+	}
+
+}
---
+++ drawroom/src/test/java/com/example/drawroom/DrawroomBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drawroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawroomBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
